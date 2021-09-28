91dd64380b106cdd1d0e0f11f6d0c1e4ede4453c
chatbot6000 <galactophage@gmail.com>
2021-09-28 09:31:21 +0200

    fixed i hope

--- Troop44/settings.py
+++ Troop44/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '8pwvdnu%15c3^+6%#9=l5k&pt^(6h)p-t-9st1izp4*(fim&zu'
+
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
